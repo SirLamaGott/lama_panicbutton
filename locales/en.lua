5628516c50585548e6c939e817ec8f5e0ebbea26
John <johnbronnert@gmail.com>
2022-11-24 15:05:28 +0100

    feat(): added english language

---
+++ locales/en.lua
@@ -0,0 +1,5 @@
+Locales['en'] = {
+    ['panicbutton'] = 'Panic Button',
+    ['positionbutton'] = 'Position Button',
+    ['waypoint'] = 'Press ~INPUT_CONTEXT~ to set a waypoint or ~INPUT_DETONATE~, to decline the request.'
+}
